f4673dcfcfea9c247a8eadd80c3b6082c2769c00
JRob <72467031+Jonathan-84@users.noreply.github.com>
2021-04-21 23:58:56 -0400

    starting to rebuild app

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const AppNavbar = () => {
+    // set modal display state
+   // const [showModal, setShowModal] = useState(false);
+  
+    return (
+      <>
+        <div className="jumbotron jumbotron-fluid bg-warning">
+    <div className="container">
+      <h1 className="display-4">So You're Saying Theres a Chance!</h1>
+      <p className="lead">A light tool to help keep you and your prospects on schedule.</p>
+    </div>
+    </div>
+      </>
+    );
+  };
+  
+  export default AppNavbar;
+  

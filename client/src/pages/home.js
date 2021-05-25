0041dbe97dca267c65e4501d9ea7f7e273e77df0
JRob <72467031+Jonathan-84@users.noreply.github.com>
2021-05-25 14:49:40 -0400

    started building out the planning page teaser

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import fromNow from '../images/fromNow.jpg';
 import Deadlines from '../images/Deadlines.jpg';
+import Planning from '../images/PLanningPage.jpg';
 
 
 
@@ -23,7 +24,9 @@ const Home = () => {
         <img className="img-fluid rounded border border-info " src={fromNow} alt="fromNow feature" />
         </div>
         <div class="col-sm d-flex align-items-center">
-          <p> Using the From Today feature, you could project the earliest day that the solution would be implemented,
+          <div className="row">
+        
+          <p> Using the <span className="font-weight-bold">From Today</span> feature, you could project the earliest day that the solution would be implemented,
             if an agreement was signed today.
           </p>
         </div>
@@ -35,11 +38,28 @@ const Home = () => {
         <img className="img-fluid rounded border border-info" src={Deadlines} alt="Deadlines Feature" />
         </div>
         <div class="col-sm d-flex align-items-center">
-        <p> Using the Deadlines feature, identify your client's ideal start date then reverse the acqusition 
+        <p> Using the <span className="font-weight-bold">Future Deadlines</span> feature, identify your client's ideal start date then reverse the acqusition 
           steps frome there. In particular, let's determine deadlines for the Implementation to begin and the 
           Legal Review to end.
           </p>
         </div>
+        </div>
+        <br></br>
+        <br></br>
+        <div class="row">
+    <div class="col-sm">
+    
+        <img className="img-fluid rounded border border-info " src={Planning} alt="Planning feature" />
+        </div>
+        <div class="col-sm d-flex align-items-center">
+          <p> Sneak Peak at a future update, the <span className="font-weight-bold">Planning</span> feature... The Acquisition and Sales process is riddled with multi-step processes.
+            Whether they are RFPs or the actual Implementation Process, this tool will help. It gives you the ability to build out 
+            multiple deadlines from today.
+          </p>
+        </div>
+        </div>
+        <br></br>
+        <br></br>
         </div>
         </div>
         <br></br>

517b981361fbdb7167c27225b9655f19c5917cd1
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2025-02-06 11:54:20 -0500

    attempting to fix the build

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/project-gates',
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: true,
+  useFindAndModify: false,
   family: 4,
 });
 
